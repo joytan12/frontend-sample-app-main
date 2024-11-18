cdf49bd94213f92cd702eabd89f29cf412bdfc37
ElVeexo <vicente25852@gmail.com>
2024-11-18 00:12:51 -0300

    fix error botón gráficos

--- src/components/nav_bar.jsx
+++ src/components/nav_bar.jsx
@@ -7,24 +7,41 @@ import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   const location = useLocation();
-  const isHomePage = location.pathname === '/';
+
+  // Determinar la ruta del botón dinámico
+  const getArrowDestination = () => {
+    if (location.pathname === '/Altura') {
+      return '/Psicomotor'; // Si está en /Altura, enviar a /Psicomotor
+    }
+    if (location.pathname === '/Psicomotor') {
+      return '/Altura'; // Si está en /Psicomotor, enviar a /Altura
+    }
+    return null; // No mostrar el botón en otras páginas
+  };
+
+  const arrowDestination = getArrowDestination();
 
   return (
     <nav className="nav-bar">
+      {/* Logo */}
       <div className="nav-bar__logo">
         <Link to="/">
           <img src={logo} alt="Logo" />
         </Link>
       </div>
+
+      {/* Título */}
       <div className="nav-bar__title">
         <h1>Creciendo Juntos</h1>
       </div>
-      {!isHomePage && (
-        <a href="# ">
+
+      {/* Botón Arrow dinámico */}
+      {arrowDestination && (
+        <Link to={arrowDestination}>
           <div className="nav-bar__image">
             <img src={arrow} alt="Arrow Icon" />
           </div>
-        </a>
+        </Link>
       )}
     </nav>
   );
